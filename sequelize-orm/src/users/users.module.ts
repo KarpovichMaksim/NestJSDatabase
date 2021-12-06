f642f5c94863c1a9e1a16dcb2fe4960ad70df1c8
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-06 23:12:50 +0300

    Create users controller

--- sequelize-orm/src/users/users.module.ts
+++ sequelize-orm/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './user.model';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [SequelizeModule.forFeature([User])],
+  controllers: [UsersController],
 })
 export class UsersModule {}
