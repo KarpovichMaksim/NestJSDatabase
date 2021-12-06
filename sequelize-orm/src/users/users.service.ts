9946466ebbc704dbdf497e90c80048ad6ddad6b6
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-06 23:18:07 +0300

    Add transaction in users service

--- sequelize-orm/src/users/users.service.ts
+++ sequelize-orm/src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize-typescript';
 import { User } from './user.model';
 
 @Injectable()
@@ -7,7 +8,25 @@ export class UsersService {
   constructor(
     @InjectModel(User)
     private userModel: typeof User,
+    private sequelize: Sequelize,
   ) {}
+
+  async createMany() {
+    try {
+      await this.sequelize.transaction(async (t) => {
+        const transactionHost = { transaction: t };
+
+        await this.userModel.create(
+          { firstName: 'Abraham', lastName: 'Lincoln' },
+          transactionHost,
+        );
+        await this.userModel.create(
+          { firstName: 'John', lastName: 'Boothe' },
+          transactionHost,
+        );
+      });
+    } catch (err) {}
+  }
 
   async findAll(): Promise<User[]> {
     return this.userModel.findAll();
