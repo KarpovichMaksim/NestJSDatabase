51958333cebf84a63e92d65f02589ee0c5bc4de9
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-08 22:50:49 +0300

    Add service method

--- mongoose/src/cats/cats.service.ts
+++ mongoose/src/cats/cats.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat, CatDocument } from 'src/schemas/cat.schema';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  constructor(@InjectModel(Cat.name) private catModel: Model<CatDocument>) {}
+
+  async create(createCatDto: CreateCatDto): Promise<Cat> {
+    const createCat = new this.catModel(createCatDto);
+    return createCat.save();
+  }
+
+  async findAll(): Promise<Cat[]> {
+    return this.catModel.find().exec();
+  }
+}
