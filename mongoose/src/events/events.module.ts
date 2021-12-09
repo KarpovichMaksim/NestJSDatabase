8577238b8762f4d6c4b2b4cdc7d6b79ed0706ee4
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-09 23:23:08 +0300

    Add change in events module

--- mongoose/src/events/events.module.ts
+++ mongoose/src/events/events.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  ClickedLinkEvent,
+  ClickedLinkEventSchema,
+} from './click-link-event.schema';
+import { EventSchema } from './event.schema';
+import { SignUpEvent, SignUpEventSchema } from './sign-up-event.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Event.name,
+        schema: EventSchema,
+        discriminators: [
+          { name: ClickedLinkEvent.name, schema: ClickedLinkEventSchema },
+          { name: SignUpEvent.name, schema: SignUpEventSchema },
+        ],
+      },
+    ]),
+  ],
+})
 export class EventsModule {}
